fe6d48a996745d35470ec6aaf33e2c6e2f0c23fb
Atharv724 <98732673+Atharv724@users.noreply.github.com>
2022-05-09 20:04:17 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+canvas = document.getElementById("mycanvas");
+ctx  = canvas.getContext("2d");
+color = "red";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown",mouse);
+function mouse(e){
+    color = document.getElementById("color").value;
+    mousex = e.clientX - canvas.offsetLeft;
+    mousey = e.clientY - canvas.offsetTop;
+    circle(mousex,mousey);
+
+}
+function circle(mousex, mousey){
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 2;
+    ctx.arc(mousex, mousey, 40, 0, 2*Math.PI);
+    ctx.stroke();   
+}
+function clearArea(){
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+}
